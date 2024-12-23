4b0f0fe5f722adc20bdffa54d3f1d96c2d41c817
ShabanovIV <react.dev.shabanov@mail.ru>
2024-12-23 09:41:56 +0000

    Deploying to gh-pages from @ ShabanovIV/ShabanovIV.github.io@6adf35ffd6f5e594f9c486405ef932de8dbc3f52 🚀

--- sb-addons/storybook-9/manager-bundle.js
+++
@@ -1,3 +0,0 @@
-try{
-(()=>{var p=__STORYBOOK_ADDONS__,{addons:O,types:f,mockChannel:l}=__STORYBOOK_ADDONS__;O.setConfig({showToolbar:!1});})();
-}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
